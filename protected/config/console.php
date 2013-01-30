60775d4cba4b111692383e0c016ed33563ec0776
Leopoldo Rojas Moguel <leopoldo.rojas@gmail.com>
2013-01-29 18:13:05 -0600

    Initial commit of console

--- protected/config/console.php
+++ protected/config/console.php
@@ -4,7 +4,7 @@
 // Any writable CConsoleApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Microfinzas - Movimientos de Operaciones',
+	'name'=>'Microfinzas - Movimientos de Operaciones de IMF',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
