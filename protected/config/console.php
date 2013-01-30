0d13d7fa66fad8a5e18a7d251b63fff57d673ecf
Leopoldo Rojas Moguel <leopoldo.rojas@gmail.com>
2013-01-29 18:34:46 -0600

    Resolviendo conflicto 2

--- protected/config/console.php
+++ protected/config/console.php
@@ -4,13 +4,7 @@
 // Any writable CConsoleApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-<<<<<<< HEAD
-	'name'=>'Microfinzas - Movimientos de Operaciones de IMF',
-	// Actualización de master. Este es el master.
-=======
 	'name'=>'Microfinzas - Movimientos de Operaciones',
-	// Este es la actualización del branch development. Este es development.
->>>>>>> development
 
 	// preloading 'log' component
 	'preload'=>array('log'),
--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Sistema de Operaciones para IMFs',
+	'name'=>'Sistema de Operaciones para Instituciones Microfinancieras',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
