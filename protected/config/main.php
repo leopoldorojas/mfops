62af0792bc6750c7bfd78dd2458694a6375fad93
Leopoldo Rojas Moguel <leopoldo.rojas@gmail.com>
2013-01-29 18:07:28 -0600

    initial commit on development

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Sistema de Operaciones para Instituciones Microfinancieras',
+	'name'=>'Sistema de Operaciones para IMFs',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
